f48c1d9ad8da96d4dfb4ca92107468bd00e61575
purple_flood <49845374+PurpleFlood@users.noreply.github.com>
2024-04-18 10:30:28 +0200

    Initial Commit

---
+++ ransomware_C_macos/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ransomware_C_macos
+//
+//  Created by asap on 18/04/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
