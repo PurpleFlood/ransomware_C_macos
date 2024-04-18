4e625fe4ac894613aa12e8b25e62c28577ce8f77
purple_flood <49845374+PurpleFlood@users.noreply.github.com>
2024-04-18 11:01:39 +0200

    Update main.c

--- ransomware_C_macos/main.c
+++ ransomware_C_macos/main.c
@@ -6,9 +6,20 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include "encrypt_decrypt.h"
+#include "list_dir.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    char* tableau[100];
+    char* location="/Users/asap/Desktop/";
+    list_file_dir(location);
+    printf("%s", tableau);
+    for(int i=0;i<100;i++){
+        printf("%s\n", tableau[i]);
+    }
+    
     return 0;
 }
