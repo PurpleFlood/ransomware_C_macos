9645c59350095138292508b84a29659e2900a8fa
purple_flood <49845374+PurpleFlood@users.noreply.github.com>
2024-04-26 19:02:35 +0200

    Update list_dir.h

--- ransomware_C_macos/list_dir.h
+++ ransomware_C_macos/list_dir.h
@@ -8,7 +8,21 @@
 #ifndef list_dir_h
 #define list_dir_h
 
-
+char* list_file_dir(char* location){
+    struct dirent *dir;
+    DIR *d=opendir(location);
+    char* output[100];
+    int i=0;
+    if(d){
+        while((dir=readdir(d))!=NULL){
+            output[i]=dir->d_name;
+            printf("%s\n", output[i]);
+            i++;
+        }
+    }
+    closedir(d);
+    return output[];
+}
 
 
 #endif /* list_dir_h */
